68f931ac781b621a3b50697522fd08d83468fe40
Rafał Majewski <goodheropl@gmail.com>
2022-01-03 20:52:28 +0100

    createGetFromQuery for float and integers

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const symbols = require("./symbols.js");
 const createServer = require("./createServer.js");
 const middlewares = require("./middlewares.js");
+const createGetFromQuery = require("./createGetFromQuery.js");
 
 module.exports = {
 	createServer,
 	symbols,
 	middlewares,
+	createGetFromQuery,
 };
