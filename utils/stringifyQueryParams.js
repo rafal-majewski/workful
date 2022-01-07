600526013554603aecd721811a75c94016cde07e
Rafał Majewski <goodheropl@gmail.com>
2022-01-07 21:58:20 +0100

    stringifyQueryParams fix for null valies + tests

--- utils/stringifyQueryParams.js
+++ utils/stringifyQueryParams.js
@@ -1,3 +1,9 @@
-const stringifyQueryParams = (queryParams) => (Object.entries(queryParams).map(([key, value]) => `${key}=${value}`).join("&"));
+const stringifyQueryParams = (queryParams) => (
+	Object.entries(queryParams).map(
+		([key, value]) => (
+			`${key}${value == null ? "" : `=${value}`}`
+		)
+	).join("&")
+);
 
 module.exports = stringifyQueryParams;
