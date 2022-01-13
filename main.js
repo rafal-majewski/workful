2b359e511ccb79c2ddd2c51bc8e0a507bbc327cc
Rafał Majewski <goodheropl@gmail.com>
2022-01-13 21:59:32 +0100

    fix wrong export name

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const symbols = require("./utils/methodsSymbols.js");
+const methodsSymbols = require("./utils/methodsSymbols.js");
 const createServer = require("./createServer.js");
 const middlewares = require("./utils/middlewares.js");
 // const static = require("./static.js");
 
 module.exports = {
 	createServer,
-	symbols,
+	methodsSymbols,
 	middlewares,
 	// static,
 };
