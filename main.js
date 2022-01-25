12d464169ade05a1c31f977d19f658af3ca26279
Rafał Majewski <goodheropl@gmail.com>
2022-01-25 01:19:56 +0100

    expose errors

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const methodsSymbols = require("./utils/methodsSymbols.js");
 const createServer = require("./createServer.js");
 const middlewares = require("./utils/middlewares.js");
+const errors = require("./utils/errors.js");
 // const static = require("./static.js");
 
 module.exports = {
 	createServer,
 	methodsSymbols,
 	middlewares,
+	errors,
 	// static,
 };
